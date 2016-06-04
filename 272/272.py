99c462c2c25e7c04be25a6873b831c1dfbc0f58e
David Cardozo <David Cardozo>
2016-06-04 00:17:02 -0500

    Getting used to UVA

---
+++ 272/272.py
@@ -0,0 +1,14 @@
+import sys
+counter_closed_tick = False
+s = ""
+i = 0
+for line in sys.stdin:
+    while "\"" in line:
+        if i % 2 == 0:
+            line = line.replace("\"", "``",1)
+        elif i % 2 == 1:
+            line = line.replace("\"", "\'\'",1)
+        i += 1
+    if ( line != ""):
+        s += line
+print(s)    
